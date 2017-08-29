85a7a2bd9cbd3c664b727e4cf3644d247d546994
Andrey Okomin <oqewok@gmail.com>
2017-08-29 03:18:22 +0300

    Added classes to operate imgs loaded using file dialog, to paint on them, refactoring, and so on

--- Net/GUI/Demo/Demo.py
+++ Net/GUI/Demo/Demo.py
@@ -1,11 +1,6 @@
 import sys
-import os
 
 import PIL
-from PyQt5.QtCore import QCoreApplication, QDir, QUrl, QFileInfo
-from PyQt5.QtGui import QPixmap, QImage, QPainter, QPen
-from PyQt5.QtMultimedia import QMediaContent
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -13,62 +8,72 @@ from PyQt5.QtCore import *
 
 from Net.GUI.Demo.mainwindow import Ui_MainWindow as MainWindow
 
+
 class MainForm(QMainWindow, MainWindow):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         super(MainForm, self).__init__(parent)
         self.setupUi(self)
 
-        self.imageFrame = Frame()
+        '''btn_Quit.OnClick -> Close'''
+        self.btn_Quit.clicked.connect(self.OnCloseWindowClick)
 
-        self.btn_OpenFile.clicked.connect(self.load_with_file_dialog)
+        '''btn_OpenFile -> Open File Dlg'''
+        self.btn_OpenFile.clicked.connect(self.OnLoadWithFileDialogClick)
 
+        self.btn_FrameRecognize.clicked.connect(self.OnRecognizeClick)
 
+        '''Create instance to store loaded img'''
+        self.imageContainer = Frame()
 
-
-
-        self.myPixmap = QPixmap("D:\\FFOutput\\1234m.png")
-        self.myPixmap = QPixmap("D:\\output_pic1.jpg")
-        self.label_Image.setPixmap(self.myPixmap)
-
-
-        # Изображение до размера label и показать
-        self.label_Image.setGeometry(0, 0, 800, 452)
-        self.myPixmap = self.myPixmap.scaled(self.label_Image.size(), Qt.KeepAspectRatio)
-        self.label_Image.setPixmap(self.myPixmap)
-        # self.label_Image.setScaledContents(True)
-
-        self.label_Image.show()
-
-        self.painter = QPainter(self.myPixmap)
-
-        self.pen = QPen(Qt.red, 0)
-        self.painter.setPen(self.pen)
-        self.painter.drawLine(self.label_Image.x(), self.label_Image.x(), 400, 400)
-
-        self.label_Image.setPixmap(self.myPixmap)
+        '''Set img preferred size'''
+        self.init_label()
         pass
 
+    def init_label(self):
+        self.label_Image.setGeometry(0, 0, 800, 452)
+        pixm = QPixmap(self.label_Image.size())
+        pixm = pixm.scaled(self.label_Image.size(), Qt.KeepAspectRatio)
+        self.label_Image.setPixmap(pixm)
+        pass
 
+    def update_label_Image(self, pixmap):
+        self.label_Image.setPixmap(pixmap)
 
+    # todo doesnt work, pass stub
     '''Закрыть инстанс GUI'''
-    def close_window(self):
-        QCoreApplication.Core.instance().quit()
 
+    def OnCloseWindowClick(self):
+        # QCoreApplication.Core.instance().quit()
+        pass
 
     '''Загрузить изображение'''
-    def load_with_file_dialog(self):
+
+    def OnLoadWithFileDialogClick(self):
         try:
             file_name, _ = QFileDialog.getOpenFileName(self, "Выберите файл")
             file_info = QFileInfo(file_name)
 
             if file_info.exists():
-                path = file_info.absoluteFilePath()
-                self.imageFrame.data = QImage(path)
+                # How to get original name and full name
+                # originalName = QFileInfo.completeBaseName(file_info)
+                # QFileInfo.baseName()
 
-                # self.imageFrame.save("D:\\out.jpg")
-                self.imageFrame.save_image(fileFullPath="D:\\output_pic1.jpg")
-                self.imageFrame.setPicture()
-                self.imageFrame.picture.save("D:\\pic_out.jpg")
+                path = file_info.absoluteFilePath()
+                originalExtenstion = QFileInfo.completeSuffix(file_info)
+
+                self.imageContainer.set_picture(path)
+                self.imageContainer.originalExtension = originalExtenstion
+
+                self.imageContainer.dataCopy = self.imageContainer.dataCopy.scaled(self.label_Image.size(),
+                                                                                   Qt.KeepAspectRatio)
+                self.update_label_Image(self.imageContainer.dataCopy)
+
+                '''Uncomment to draw example on img loaded'''
+                # painter  = Painter(self.imageContainer.dataCopy, Qt.blue, 10)
+                # painter.paint_line(90, 20, 90, 300)
+                # painter.paint_rectangle(0, 250, 400, 35)
+                # self.update_label_Image(self.imageContainer.dataCopy)
+
                 return path
 
             else:
@@ -79,37 +84,75 @@ class MainForm(QMainWindow, MainWindow):
             return
         pass
 
-    # 1
-    #'''Нарисовать линии'''
-    # def paintEvent(self, event):
-    #     painter = QPainter(self)
-    #     self.pixmap = QPixmap("D:\\pic_out.jpg")
-    #     painter.drawPixmap(self.rect(), self.pixmap)
-    #     pen = QPen(Qt.red, 3)
-    #     painter.setPen(pen)
-    #     painter.drawLine(10, 10, self.rect().width() - 10, 10)
+    '''Stub method'''
+
+    def OnRecognizeClick(self):
+        pass
 
 
-
-class Frame(QImage):
+class Frame():
     def __init__(self, parent=None):
-        super(Frame, self).__init__(parent)
+        '''Placeholder to keep image loaded further'''
+        self.originalExtension = None
 
-        self.data = None
+        self.data = QPixmap()
 
-        self.picture = QPixmap()
+        '''Contains a copy of data at some moment'''
+        self.dataCopy = QPixmap()
+
+    '''Construct internal pixmap from QPixmap'''
+
+    def set_picture(self, QPixmap):
+        self.data = QPixmap(QPixmap)
+        self.dataCopy = self.data.copy()
+
+    '''Construct internal pixmap from path str'''
+
+    def set_picture(self, FullPathToImg):
+        self.data = QPixmap(FullPathToImg)
+        self.dataCopy = self.data.copy()
+
+    # todo doesn't work =(
+    '''Save unmodified data img'''
+
+    def save_image(self, fileFullPath=(QDir.currentPath() + "\\"),
+                   fileName="default",
+                   extension="jpg"):
+        b = self.originalExtension  # example "png"
+        pass
+
+        path = fileFullPath + fileName + "." + extension
+        self.data.save(path)
 
 
-    def setPicture(self):
-        self.picture = QPixmap(self.data)
+class Painter():
+    def __init__(self, whereToDraw=None, penColour=None, penSize=None):
+        self.painter = QPainter(whereToDraw)
+        self.pen = QPen(Qt.red, 3)
+        self.painter.setPen(self.pen)
+
+    def paint_line(self, x1, y1, x2, y2):
+        self.painter.drawLine(x1, y1, x2, y2)
+        pass
+
+    def paint_rectangle(self, x1, y1, width, height):
+        self.painter.drawRect(x1, y1, width, height)
+        pass
 
 
-    def save_image(self, fileFullPath = "D:\\out.jpg"):
-        self.data.save(fileFullPath)
+''' "C:\\Users\\Username" '''
 
 
 def getHomePath():
     return QDir.homePath()
+
+
+''' The place where this .py exists'''
+
+
+def getCurrentPath():
+    return QDir.currentPath()
+
 
 def main():
     application = QApplication(sys.argv)
